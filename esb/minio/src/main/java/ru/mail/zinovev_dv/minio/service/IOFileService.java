f6373c8c16639f0c77795b7dc4128976d431b1ec
Zinovev Dmitrii <Zinovev-DV@mail.ru>
2023-07-12 14:04:28 +0300

    add minio srvice

--- esb/minio/src/main/java/ru/mail/zinovev_dv/minio/service/IOFileService.java
+++ esb/minio/src/main/java/ru/mail/zinovev_dv/minio/service/IOFileService.java
@@ -1,6 +1,13 @@
 package ru.mail.zinovev_dv.minio.service;
 
+import io.minio.errors.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public interface IOFileService {
-    public void saveFile(String path, String filename, FileInputStream fileInputStream);
-    public InputStream getFile(String path, String filename);
+    void saveFile(String path, String filename, InputStream fileInputStream) throws ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException, InvalidResponseException, IOException, NoSuchAlgorithmException, ServerException, XmlParserException;
+    InputStream getFile(String path, String filename) throws IOException, ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException;
 }
--- esb/minio/src/main/java/ru/mail/zinovev_dv/minio/service/Minio.java
+++ esb/minio/src/main/java/ru/mail/zinovev_dv/minio/service/Minio.java
@@ -1,35 +1,33 @@
 package ru.mail.zinovev_dv.minio.service;
 
-import io.minio.GetObjectArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.errors.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class Minio implements IOFileService {
     private final MinioClient minioClient;
 
     public Minio(String host, Integer port, String login, String password){
-        String endpoint = StringBuilder
-                .append(host)
-                .append(":")
-                .append(port.ToString())
-                .ToString();
         minioClient = MinioClient.builder()
                 .credentials(login, password)
-                .endpoint(endpoint)
+                .endpoint(host, port, false)
                 .build();
     }
-    public void saveFile(String path, String filename, FileInputStream fileInputStream){
-        if(!minioClient.bucketExists(path)
+    public void saveFile(String path, String filename, InputStream inputStream) throws ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException, InvalidResponseException, IOException, NoSuchAlgorithmException, ServerException, XmlParserException {
+        if(!minioClient.bucketExists(BucketExistsArgs.builder().bucket(path).build()))
             minioClient.makeBucket(MakeBucketArgs.builder().bucket(path).build());
 
-        minioClient.putObject(PutObjectArgs.builder()
-                        .bucket(path)
-                        .object(filename)
-                        .stream(fileInputStream)
-                .build());
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                    .bucket(path)
+                    .object(filename)
+                    .stream(inputStream, -1, -1).build());
     }
-    public InputStream getFile(String path, String filename){
+    public InputStream getFile(String path, String filename) throws IOException, ErrorResponseException, InsufficientDataException, InternalException, InvalidKeyException, InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException{
         try(InputStream stream =
                 minioClient.getObject(GetObjectArgs.builder()
                                 .bucket(path)
@@ -38,6 +36,4 @@ public class Minio implements IOFileService {
             return stream;
         }
     }
-
-
 }
