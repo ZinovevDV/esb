59ff66b2f89249b88fb815d2bfc2a16e2565066a
Zinovev Dmitrii <Zinovev-DV@mail.ru>
2023-07-13 00:52:06 +0300

    add javadoc

--- esb/minio/src/main/java/ru/mail/zinovev_dv/minio/config/ConfigurationPropertiesMinio.java
+++ esb/minio/src/main/java/ru/mail/zinovev_dv/minio/config/ConfigurationPropertiesMinio.java
@@ -4,15 +4,30 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * Конфигурация minio.
+ */
 @ConfigurationProperties(prefix = "minio")
 @Getter
 @Setter
 public class ConfigurationPropertiesMinio {
+    /**
+     * Адрес minio.
+     */
     private String host;
 
+    /**
+     * Порт minio.
+     */
     private Integer port;
 
+    /**
+     * Пользователь подключения к minio.
+     */
     private String login;
 
+    /**
+     * Пароль пользователя подключения к minio.
+     */
     private String password;
 }
